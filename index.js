0dd30dcbc998a8e84b9986d9a8af04d31e9fabdf
Web Development <web@btech.edu>
2026-06-17 09:03:01 -0600

    syles.css styles update

--- index.js
+++ index.js
@@ -36,5 +36,10 @@ qaItems.forEach((qaItem) => {
 
 	questionDiv.appendChild(answerDiv);
 
+	questionDiv.addEventListener("click", () => {
+		questionDiv.classList.toggle("active");
+		answerDiv.classList.toggle("active");
+	});
+
 	accordionDiv.appendChild(questionDiv);
 });
